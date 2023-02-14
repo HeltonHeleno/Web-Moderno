2f2044b943a1822c5fbbc570a180042ce6b1b1e9
Helton Heleno <helton_heleno@hotmail.com>
2023-02-13 21:30:46 -0300

    aula 91

--- src/4_funcao/91_parametros_padrao.js
+++ src/4_funcao/91_parametros_padrao.js
@@ -13,4 +13,14 @@ function soma2(a, b, c) {
   a = a !== undefined ? a : 1
   b = 1 in arguments ? b: 1
   c = isNaN(c) ? 1 : c
+  return a + b + c
 }
+
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
+
+// valor padrão do es2015
+function soma3 (a =1, b = 2, c =3){
+  return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0))
