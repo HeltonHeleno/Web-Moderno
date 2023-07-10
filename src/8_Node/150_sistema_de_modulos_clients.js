9013604568b332365637d209dae772d360a33488
Helton Heleno <helton_heleno@hotmail.com>
2023-07-10 08:41:14 -0300

    até a aula 150

---
+++ src/8_Node/150_sistema_de_modulos_clients.js
@@ -0,0 +1,11 @@
+const moduloA = require('./150_sistema_de_modulos')
+const moduloB = require('./150_sistema_de_modulos_2')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
