3eefbe0498b5e376fce13b89bf0c4075d3b8c5b4
Helton Heleno <helton_heleno@hotmail.com>
2023-08-31 07:33:38 -0300

    aula final de javaScript

--- src/9_ESNext/197_async_await_02.js
+++ src/9_ESNext/197_async_await_02.js
@@ -1,8 +1,32 @@
-function gerarNumerosEntre(min, max) {
+function gerarNumerosEntre(min, max, numerosProibidos) {
   if(min > max) [max, min] = [min, max]  
-  return new Promise(resolve => {
-    const fator = max - min +1
+  return new Promise((resolve,, reject) => {
+    const fator = max - min + 1
     const aleatorio = parseInt(Math.random() * fator) + min
-    resolve (aleatorio)
+    if(numerosProibidos.includes(aleatorio)){
+      reject('Numeros Repetidos')
+    } else{
+      resolve (aleatorio)
+    }
   })
 }
+
+async function gerarMegaSena(qtdNumeros, tentativas = 1) {
+  try {
+    const numeros = []
+    for(let _ of Array(qtdNumeros).fill()){
+      numeros.push(await gerarNumerosEntre(1, 60, numeros))
+    }
+    return numeros    
+  } catch (e) {
+    if(tentativas > 10){
+      throw "Não deu certo!"
+    }else {
+      return gerarMegaSena(qtdNumeros, tentativas + 1)
+    }
+  }
+}
+
+gerarMegaSena(8)
+  .then(console.log)
+  .catch(console.log)
